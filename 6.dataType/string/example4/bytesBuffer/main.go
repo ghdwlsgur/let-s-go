a6a1375aa194f38b4c141b3e0ddb42bdf28a432b
HongJinHyeok <redmax45@naver.com>
2022-05-18 12:35:31 +0900

    ✨ 문자열 연결 (bypes.Buffer)

---
+++ 6.dataType/string/example4/bytesBuffer/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"math"
+	"unicode"
+)
+
+// ! bytes.Buffer 사용
+func main() {
+	var buffer bytes.Buffer
+	for i := 0; i < math.MaxUint8; i++ {
+		if s, ok := nextString(i); ok {
+			buffer.WriteString(s)
+		}
+	}
+	fmt.Print(buffer.String(), "\n")
+}
+
+func nextString(i int) (s string, ok bool) {
+	if unicode.IsLetter(rune(i)) {
+		return string(i), true
+	}
+	return "", false
+}
