0822c6a6af48f79bfcb3034590be5ad6f4c4a795
HongJinHyeok <redmax45@naver.com>
2022-05-18 12:35:49 +0900

    ✨ 문자열 연결 (strings.Join)

---
+++ 6.dataType/string/example4/stringsJoin/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+	"unicode"
+)
+
+// ! strings.Join 사용
+func main() {
+	strArr := []string{}
+	for i := 0; i < math.MaxUint8; i++ {
+		if s, ok := nextString(i); ok {
+			strArr = append(strArr, s)
+		}
+	}
+	fmt.Println(strings.Join(strArr, ""))
+
+}
+
+func nextString(i int) (s string, ok bool) {
+	if unicode.IsLetter(rune(i)) {
+		return string(i), true
+	}
+	return "", false
+}
