6d54b0d604df9a1828d4f546733c83bdf9a387b6
HongJinHyeok <redmax45@naver.com>
2022-05-19 12:07:12 +0900

    ✨ go 배열

---
+++ 6.dataType/array/array.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a [5]int
+	b := [3]int{1, 2, 3}
+	c := [3]int{1, 2}
+	d := [...]int{4, 5, 6, 7, 8}
+	e := [3][3]int{{1, 2, 3}, {3, 4, 5}}
+
+	fmt.Printf("%10T %d %v\n", a, len(a), a)
+	fmt.Printf("%10T %d %v\n", b, len(b), b)
+	fmt.Printf("%10T %d %v\n", c, len(c), c)
+	fmt.Printf("%10T %d %v\n", d, len(d), d)
+	fmt.Printf("%10T %d %v\n", e, len(e), e)
+
+}
