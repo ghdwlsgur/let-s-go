d73b2d9673530aeccd89682480d97e080f60b18e
HongJinHyeok <redmax45@naver.com>
2022-05-29 00:17:58 +0900

    👔 backjoon 3052번

--- backjoon/phase_4/4/main.go
+++ backjoon/phase_4/4/main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
 )
 
 func main() {
@@ -12,61 +11,18 @@ func main() {
 	var wr *bufio.Writer = bufio.NewWriter(os.Stdout)
 	defer wr.Flush()
 
-	var arr = make([]int, 10)
-	var comparison = make([]int, len(arr))
+	var arr = make([]int, 42)
 
-	var n, cnt int = 0, 1
+	var n, cnt int
 	for i := 0; i < 10; i++ {
 		fmt.Fscanln(rd, &n)
-		k := n % 42
-		arr[i] += k
+		arr[n%42]++
 	}
 
-	fmt.Println(arr)
-	// 39 40 41 0 1 2 40 41 0 1
-	// [k] [k]
-	// 39 40 41 0 1 2 40 41 0 1
-	// 40
-	// 39 41 0 1 2 41 0 1
-	// 41
-	// 39 0 1 2 0 1
-	// 0
-	// 39 1 2 1
-	// 1
-	// 39 2
-
-	// 39
-	//
-	// 39 -> 40 41 0 1 2 ...
-	// 40 -> 41 0 1 2 40 41 0 1
-	// 41 -> 0 1 2 40 41 0 1
-	//
-
-	copy(comparison, arr)
-	fmt.Println(comparison)
-
-	for i,v := range arr {
-		for j := i + 1; i <= 9 j++ {
-			
+	for i := range arr {
+		if arr[i] > 0 {
+			cnt++
 		}
 	}
-	// arr[]
-
-	// for i, v := range arr {
-	// 	fmt.Println("before", arr)
-	// 	for j := i + 1; j <= 9; j++ {
-	// 		if v == arr[j] && j != 9 {
-	// 			arr = append(arr[:j], arr[j+1:]...)
-	// 			arr = append(arr[:i], arr[i+1:]...)
-	// 			j--
-	// 			i--
-	// 			fmt.Println("after", arr)
-	// 		} else if j == 9 {
-	// 			arr = append(arr[:j], arr[len(arr)-1:]...)
-	// 		}
-	// 	}
-	// 	cnt++
-	// }
-
-	wr.WriteString(strconv.Itoa(cnt))
+	fmt.Fprintln(wr, cnt)
 }
